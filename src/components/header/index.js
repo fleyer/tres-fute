12ecd22bdeeebe9936c0147c24f76ea65f6a5512
Romain Manchado <romain.manchado@bell.ca>
2021-10-19 09:23:59 -0400

    add header nav - github

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,18 @@
 import { h } from 'preact';
+
+import { Link } from 'preact-router';
+
+import { FaGithub } from 'react-icons/fa'
+
 import style from './style.css';
 
 const Header = () => (
 	<header class={style.header}>
 		<h1>{process.env.APPLICATION_TITLE}</h1>
+
+		<nav class='flex h-full justify-center items-center'>
+			<a target='_blank' href='https://github.com/fleyer/tres-fute' class='flex h-full cursor-pointer flex items-center'><FaGithub></FaGithub></a>
+		</nav>
 	</header>
 );
 
